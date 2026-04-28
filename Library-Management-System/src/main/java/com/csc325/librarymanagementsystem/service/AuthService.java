e6e890231af47d32414d33287ad790394383189f
Justin <pickleboyj@gmail.com>
2026-04-27 21:44:16 -0400

    - add: authenticate method into AuthService using FakeData so LoginController can be tested

--- Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/service/AuthService.java
+++ Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/service/AuthService.java
@@ -1,4 +1,21 @@
 package com.csc325.librarymanagementsystem.service;
 
+import com.csc325.librarymanagementsystem.data.FakeData;
+import com.csc325.librarymanagementsystem.data.FirebaseContext;
+import com.csc325.librarymanagementsystem.model.User;
+
 public class AuthService {
+    public User authenticate(FirebaseContext firebase, String identifier, String pin) {
+        for (User user : FakeData.getUsers()) {
+            if (user.getLibraryId().equals(identifier) && user.getLibraryPin().equals(pin)) {
+                return user;
+            }
+
+            if (user.getEmail().equalsIgnoreCase(identifier) && user.getLibraryPin().equals(pin)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
 }
