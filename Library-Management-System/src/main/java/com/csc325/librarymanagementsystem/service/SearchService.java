f40741174ff3751de93138b2984a570d2a07bceb
SpaceJazz64 <turkeysandwhichpunch@gmail.com>
2026-04-27 22:28:37 -0400

    dont worry about the search test its only there to test the methods

--- Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/service/SearchService.java
+++ Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/service/SearchService.java
@@ -1,4 +1,112 @@
 package com.csc325.librarymanagementsystem.service;
 
+import com.csc325.librarymanagementsystem.model.Book;
+import com.csc325.librarymanagementsystem.data.FakeData;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
 public class SearchService {
-}
+
+    private List<Book> books = new ArrayList<>();
+    //this will have to be changed once we get Firebase working
+    public void loadTestData() {
+        books.clear();
+        books.addAll(FakeData.getBooks());
+    }
+
+    public List<Book> search(String query, String type) {
+
+        List<Book> results = new ArrayList<>();
+
+        if (query == null || query.isEmpty() || books == null) {
+            return results;
+        }
+
+        query = query.trim().toLowerCase();
+        type = type.trim().toLowerCase();
+        //huge switch case that filters the search
+        for (Book book : books) {
+
+            switch (type.toLowerCase()) {
+
+                case "title": {
+                    String[] queryWords = query.split("\\s+");
+                    String[] titleWords = book.getTitle().toLowerCase().split("\\s+");
+
+                    int matchCount = 0;
+
+                    for (String qWord : queryWords) {
+                        for (String tWord : titleWords) {
+                            if (levenshtein(qWord, tWord) <= 2) {
+                                matchCount++;
+                                break;
+                            }
+                        }
+                    }
+
+                    if (matchCount >= Math.max(1, queryWords.length / 2)) {
+                        results.add(book);
+                    }
+
+                    break;
+                }
+
+                case "author": {
+                    for (String author : book.getAuthors()) {
+                        if (author.toLowerCase().contains(query)
+                                || levenshtein(query, author.toLowerCase()) <= 2) {
+                            results.add(book);
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+
+                case "genre": {
+                    for (String genre : book.getGenres()) {
+                        if (genre.toLowerCase().contains(query)) {
+                            results.add(book);
+                            break;
+                        }
+                    }
+
+                    break;
+                }
+
+                case "isbn": {
+                    if (book.getIsbn().equals(query)) {
+                        results.add(book);
+                    }
+
+                    break;
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public int levenshtein(String a, String b) {
+        int[][] dp = new int[a.length() + 1][b.length() + 1];
+
+        for (int i = 0; i <= a.length(); i++) dp[i][0] = i;
+        for (int j = 0; j <= b.length(); j++) dp[0][j] = j;
+
+        for (int i = 1; i <= a.length(); i++) {
+            for (int j = 1; j <= b.length(); j++) {
+
+                int cost = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1;
+
+                dp[i][j] = Math.min(
+                        Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1),
+                        dp[i - 1][j - 1] + cost
+                );
+            }
+        }
+
+        return dp[a.length()][b.length()];
+    }
