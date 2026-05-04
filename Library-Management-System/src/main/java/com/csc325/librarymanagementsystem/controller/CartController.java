9f897342820079fb3214c0a72b5d397154e4011c
OscarZ9 <147277675+OscarZ9@users.noreply.github.com>
2026-05-04 14:37:48 -0400

    Finished CartScreen.fxml and it is connected to the MainScreen

--- Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/controller/CartController.java
+++ Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/controller/CartController.java
@@ -1,4 +1,31 @@
 package com.csc325.librarymanagementsystem.controller;
 
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+
 public class CartController {
+
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private void onHomeClicked(){
+        try{
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("/com/csc325/librarymanagementsystem/MainScreen.fxml")
+            );
+            Parent root = loader.load();
+            Stage stage = (Stage) homeButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
