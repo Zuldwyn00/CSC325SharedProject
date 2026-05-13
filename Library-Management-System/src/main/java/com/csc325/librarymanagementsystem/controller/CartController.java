46320edddc9c35818ef04153b28c0ced06273e5a
OscarZ9 <147277675+OscarZ9@users.noreply.github.com>
2026-05-13 15:56:45 -0400

    Working on the Loan Screen and the LoanControllor

--- Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/controller/CartController.java
+++ Library-Management-System/src/main/java/com/csc325/librarymanagementsystem/controller/CartController.java
@@ -15,16 +15,23 @@ public class CartController {
     private Button homeButton;
     private Button searchButton;
 
+    @FXML
+    private void onSearchClicked(){
+        navigateTo("/com/csc325/librarymanagementsystem/SearchScreen.fxml", searchButton);
+    }
+
+    @FXML
+    private void onHomeClicked() {
+        navigateTo("/com/csc325/librarymanagementsystem/MainScreen.fxml", homeButton);
+    }
 
 
     @FXML
-    private void onHomeClicked(){
+    private void navigateTo(String fxmlPath, Button source){
         try{
-            FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/com/csc325/librarymanagementsystem/MainScreen.fxml")
-            );
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlPath));
             Parent root = loader.load();
-            Stage stage = (Stage) homeButton.getScene().getWindow();
+            Stage stage = (Stage) source.getScene().getWindow();
             stage.setScene(new Scene(root));
             stage.show();
         } catch (Exception e){
